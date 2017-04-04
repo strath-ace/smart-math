da399df94e1af2b75a6e902387848bf0dbd4663d
Mateusz Polnik <mat.polnik@gmail.com>
2017-04-04 12:34:48 +0200

    Add autogenerated header

---
+++ include/smartmath.h
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*
+------------Copyright (C) 2016 University of Strathclyde--------------
+------------ e-mail: annalisa.riccardi@strath.ac.uk ------------------
+--------- Author: Annalisa Riccardi ----------------------------------
+*/
+
+#ifndef SMART_SMARTMATH_H
+#define SMART_SMARTMATH_H
+
+#define SMARTMATH_VERSION_MAJOR 0
+#define SMARTMATH_VERSION_MINOR 1
+#define SMARTMATH_VERSION_PATCH 0
+
+#include "LinearAlgebra/smartmath_la.h"
+#include "Dynamics/smartmath_dynamics.h"
+#include "Events/smartmath_events.h"
+#include "Integrators/smartmath_integrators.h"
+#include "Utils/smartmath_utils.h"
+
+#endif /* SMART_SMARTMATH_H */
